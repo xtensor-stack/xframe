e74df848f6f4af5f66cd18e3e91be7e52fa3f1ed
Johan Mabille <johan.mabille@gmail.com>
2018-03-18 15:04:34 +0100

    sequence view

---
+++ include/xframe/xsequence_view.hpp
@@ -0,0 +1,358 @@
+/***************************************************************************
+* Copyright (c) 2017, Johan Mabille, Sylvain Corlay and Wolf Vollprecht    *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XFRAME_XSEQUENCE_VIEW_HPP
+#define XFRAME_XSEQUENCE_VIEW_HPP
+
+#include "xtl/xiterator_base.hpp"
+
+namespace xf
+{
+
+    template <class C, class S, bool is_const>
+    class xsequence_view_iterator;
+
+    /******************
+     * xsequence_view *
+     ******************/
+
+    template <class C, class S>
+    class xsequence_view
+    {
+    public:
+
+        using self_type = xsequence_view<C, S>;
+        using container_type = C;
+        using slice_type = S;
+
+        using value_type = typename container_type::value_type;
+        using reference = typename container_type::reference;
+        using const_reference = typename container_type::const_reference;
+        using pointer = typename container_type::pointer;
+        using const_pointer = typename container_type::const_pointer;
+        using size_type = typename container_type::size_type;
+        using difference_type = typename container_type::difference_type;
+
+        using iterator = xsequence_view_iterator<C, S, false>;
+        using const_iterator = xsequence_view_iterator<C, S, true>;
+        using reverse_iterator = std::reverse_iterator<iterator>;
+        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+        xsequence_view(container_type& data, slice_type slice) noexcept;
+
+        size_type size() const noexcept;
+        bool empty() const noexcept;
+
+        reference operator[](size_type i);
+        const_reference operator[](size_type i) const;
+
+        reference at(size_type i);
+        const_reference at(size_type i) const;
+
+        reference front();
+        const_reference front() const;
+
+        reference back();
+        const_reference back() const;
+
+        iterator begin() noexcept;
+        iterator end() noexcept;
+
+        const_iterator begin() const noexcept;
+        const_iterator end() const noexcept;
+
+        const_iterator cbegin() const noexcept;
+        const_iterator cend() const noexcept;
+
+        reverse_iterator rbegin() noexcept;
+        reverse_iterator rend() noexcept;
+
+        const_reverse_iterator rbegin() const noexcept;
+        const_reverse_iterator rend() const noexcept;
+
+        const_reverse_iterator crbegin() const noexcept;
+        const_reverse_iterator crend() const noexcept;
+
+    private:
+
+        container_type& m_data;
+        slice_type m_slice;
+    };
+
+    template <class C, class S, bool is_const>
+    struct xsequence_view_iterator_traits
+    {
+        using iterator_type = xsequence_view_iterator<C, S, is_const>;
+        using value_type = typename C::value_type;
+        using reference = std::conditional_t<is_const, typename C::const_reference, typename C::reference>;
+        using pointer = std::conditional_t<is_const, typename C::const_pointer, typename C::pointer>;
+        using difference_type = typename C::difference_type;
+    };
+
+    /***************************
+     * xseuqence_view_iterator *
+     ***************************/
+
+    template <class C, class S, bool is_const>
+    class xsequence_view_iterator
+        : public xtl::xrandom_access_iterator_base2<xsequence_view_iterator_traits<C, S, is_const>>
+    {
+    public:
+
+        using self_type = xsequence_view_iterator<C, S, is_const>;
+        using base_type = xtl::xrandom_access_iterator_base2<xsequence_view_iterator_traits<C, S, is_const>>;
+        using view_type = std::conditional_t<is_const, const xsequence_view<C, S>, xsequence_view<C, S>>;
+        using slice_type = S;
+
+        using value_type = typename base_type::value_type;
+        using reference = typename base_type::reference;
+        using pointer = typename base_type::pointer;
+        using difference_type = typename base_type::difference_type;
+        using size_type = typename slice_type::size_type;
+
+        xsequence_view_iterator() = default;
+        xsequence_view_iterator(view_type* view, size_type index);
+
+        self_type& operator++();
+        self_type& operator--();
+
+        self_type& operator+=(difference_type n);
+        self_type& operator-=(difference_type n);
+
+        difference_type operator-(const self_type& rhs) const;
+
+        reference operator*() const;
+        pointer operator->() const;
+
+        bool operator==(const self_type& rhs) const;
+        bool operator<(const self_type& rhs) const;
+
+    private:
+
+        view_type* p_view;
+        size_type m_index;
+
+        // NB: another possible implementation is to store an iterator on the underlying
+        // sequence and calls step_size(m_index) on the slice to get the increment to apply
+        // to the iterator. Benchmark is required to check which method is faster.
+    };
+
+    /*********************************
+     * xsequence_view implementation *
+     *********************************/
+
+    template <class C, class S>
+    inline xsequence_view<C, S>::xsequence_view(container_type& data, slice_type slice) noexcept
+        : m_data(data), m_slice(std::move(slice))
+    {
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::size() const noexcept -> size_type
+    {
+        return m_slice.size();
+    }
+
+    template <class C, class S>
+    inline bool xsequence_view<C, S>::empty() const noexcept
+    {
+        return size() == 0;
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::operator[](size_type i) -> reference
+    {
+        return m_data[m_slice(i)];
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::operator[](size_type i) const -> const_reference
+    {
+        return m_data[m_slice(i)];
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::at(size_type i) -> reference
+    {
+        return m_data.at(m_slice(i));
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::at(size_type i) const -> const_reference
+    {
+        return m_data.at(m_slice(i));
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::front() -> reference
+    {
+        return m_data[m_slice(0)];
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::front() const -> const_reference
+    {
+        return m_data[m_slice(0)];
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::back() -> reference
+    {
+        return m_data[m_slice(m_slice.size() - 1)];
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::back() const -> const_reference
+    {
+        return m_data[m_slice(m_slice.size() - 1)];
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::begin() noexcept -> iterator
+    {
+        return iterator(this, size_type(0));
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::end() noexcept -> iterator
+    {
+        return iterator(this, size());
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::begin() const noexcept -> const_iterator
+    {
+        return cbegin();
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::end() const noexcept -> const_iterator
+    {
+        return cend();
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::cbegin() const noexcept -> const_iterator
+    {
+        return const_iterator(this, size_type(0));
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::cend() const noexcept -> const_iterator
+    {
+        return const_iterator(this, size());
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::rbegin() noexcept -> reverse_iterator
+    {
+        return reverse_iterator(end());
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::rend() noexcept -> reverse_iterator
+    {
+        return reverse_iterator(begin());
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::rbegin() const noexcept -> const_reverse_iterator
+    {
+        return crbegin();
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::rend() const noexcept -> const_reverse_iterator
+    {
+        return crend();
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::crbegin() const noexcept -> const_reverse_iterator
+    {
+        return const_reverse_iterator(cend());
+    }
+
+    template <class C, class S>
+    inline auto xsequence_view<C, S>::crend() const noexcept -> const_reverse_iterator
+    {
+        return const_reverse_iterator(cbegin());
+    }
+
+    /******************************************
+     * xsequence_view_iterator implementation *
+     ******************************************/
+
+    template <class C, class S, bool IC>
+    inline xsequence_view_iterator<C, S, IC>::xsequence_view_iterator(view_type* view, size_type index)
+        : p_view(view), m_index(index)
+    {
+    }
+
+    template <class C, class S, bool IC>
+    inline auto xsequence_view_iterator<C, S, IC>::operator++() -> self_type&
+    {
+        ++m_index;
+        return *this;
+    }
+
+    template <class C, class S, bool IC>
+    inline auto xsequence_view_iterator<C, S, IC>::operator--() -> self_type&
+    {
+        --m_index;
+        return *this;
+    }
+
+    template <class C, class S, bool IC>
+    inline auto xsequence_view_iterator<C, S, IC>::operator+=(difference_type n) -> self_type&
+    {
+        difference_type tmp = static_cast<difference_type>(m_index) + n;
+        m_index = static_cast<size_type>(tmp);
+        return *this;
+    }
+
+    template <class C, class S, bool IC>
+    inline auto xsequence_view_iterator<C, S, IC>::operator-=(difference_type n) -> self_type&
+    {
+        difference_type tmp = static_cast<difference_type>(m_index) - n;
+        m_index = static_cast<size_type>(tmp);
+        return *this;
+    }
+
+    template <class C, class S, bool IC>
+    inline auto xsequence_view_iterator<C, S, IC>::operator-(const self_type& rhs) const -> difference_type
+    {
+        return static_cast<difference_type>(m_index) - static_cast<difference_type>(rhs.m_index);
+    }
+
+    template <class C, class S, bool IC>
+    inline auto xsequence_view_iterator<C, S, IC>::operator*() const -> reference
+    {
+        return p_view->operator[](m_index);
+    }
+
+    template <class C, class S, bool IC>
+    inline auto xsequence_view_iterator<C, S, IC>::operator->() const -> pointer
+    {
+        return &(this->operator*());
+    }
+
+    template <class C, class S, bool IC>
+    inline bool xsequence_view_iterator<C, S, IC>::operator==(const self_type& rhs) const
+    {
+        return p_view == rhs.p_view && m_index == rhs.m_index;
+    }
+
+    template <class C, class S, bool IC>
+    inline bool xsequence_view_iterator<C, S, IC>::operator<(const self_type& rhs) const
+    {
+        return p_view == rhs.p_view && m_index < rhs.m_index;
+    }
+}
+
+#endif
---
+++ test/test_xsequence_view.cpp
@@ -0,0 +1,109 @@
+/***************************************************************************
+* Copyright (c) 2017, Johan Mabille, Sylvain Corlay and Wolf Vollprecht    *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "gtest/gtest.h"
+#include "xframe/xsequence_view.hpp"
+
+namespace xf
+{
+    class slice_test
+    {
+    public:
+
+        using size_type = std::size_t;
+
+        slice_test() = default;
+        slice_test(size_type min_val, size_type max_val, size_type step) noexcept
+            : m_min(min_val), m_size(size_type(std::ceil(double(max_val - min_val) / double(step)))), m_step(step)
+        {
+        }
+
+        std::size_t size() const noexcept { return m_size; }
+        size_type operator()(size_type i) const noexcept { return m_min + i * m_step; }
+
+    private:
+
+        size_type m_min;
+        size_type m_size;
+        size_type m_step;
+    };
+
+    struct xsequence_view_feature
+    {
+        xsequence_view_feature();
+
+        using data_type = std::vector<int>;
+        using view_type = xsequence_view<data_type, slice_test>;
+        data_type m_data;
+        slice_test m_slice;
+        view_type m_view;
+    };
+
+    xsequence_view_feature::xsequence_view_feature()
+        : m_data({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }), m_slice(0, 10, 2), m_view(m_data, m_slice)
+    {
+    }
+
+    TEST(xsequence_view, size)
+    {
+        xsequence_view_feature m_feature;
+        EXPECT_EQ(m_feature.m_view.size(), 5);
+    }
+
+    TEST(xsequence_view, empty)
+    {
+        xsequence_view_feature m_feature;
+        EXPECT_FALSE(m_feature.m_view.empty());
+    }
+
+    TEST(xsequence_view, access)
+    {
+        xsequence_view_feature m_feature;
+        EXPECT_EQ(m_feature.m_view[0], 0);
+        EXPECT_EQ(m_feature.m_view[1], 2);
+        EXPECT_EQ(m_feature.m_view[2], 4);
+        EXPECT_EQ(m_feature.m_view[3], 6);
+        EXPECT_EQ(m_feature.m_view[4], 8);
+
+        EXPECT_EQ(m_feature.m_view.at(0), 0);
+        EXPECT_EQ(m_feature.m_view.at(1), 2);
+        EXPECT_EQ(m_feature.m_view.at(2), 4);
+        EXPECT_EQ(m_feature.m_view.at(3), 6);
+        EXPECT_EQ(m_feature.m_view.at(4), 8);
+        EXPECT_ANY_THROW(m_feature.m_view.at(5));
+
+        EXPECT_EQ(m_feature.m_view.front(), 0);
+        EXPECT_EQ(m_feature.m_view.back(), 8);
+    }
+
+    TEST(xsequence_view, iterator)
+    {
+        xsequence_view_feature m_feature;
+        auto iter = m_feature.m_view.begin();
+        auto citer = m_feature.m_view.cbegin();
+
+        for (size_t i = 0; i < m_feature.m_view.size(); ++i)
+        {
+            EXPECT_EQ(*iter, m_feature.m_view[i]);
+            EXPECT_EQ(*citer, m_feature.m_view[i]);
+            ++iter, ++citer;
+        }
+
+        EXPECT_EQ(iter, m_feature.m_view.end());
+        EXPECT_EQ(citer, m_feature.m_view.cend());
+
+        iter = m_feature.m_view.begin();
+        citer = m_feature.m_view.cbegin();
+        iter += 2;
+        citer += 2;
+        EXPECT_EQ(*iter, m_feature.m_view[2]);
+        EXPECT_EQ(*citer, m_feature.m_view[2]);
+        EXPECT_EQ(m_feature.m_view.end() - iter, 3);
+        EXPECT_EQ(m_feature.m_view.cend() - citer, 3);
+    }
+}
