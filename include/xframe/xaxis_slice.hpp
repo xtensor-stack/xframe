14fc98a3eca96a9d71572de41ce95de41598c341
Johan Mabille <johan.mabille@gmail.com>
2018-04-11 14:33:13 +0200

    moved to xtl::make_overload

--- include/xframe/xaxis_slice.hpp
+++ include/xframe/xaxis_slice.hpp
@@ -323,43 +323,13 @@ namespace xf
     {
     }
 
-    namespace detail
-    {
-        // TODO: remove this when xtl has been released
-        template <class... Ts>
-        struct overloaded;
-
-        template <class T>
-        struct overloaded<T> : T
-        {
-            overloaded(T arg) : T(arg) {}
-            using T::operator();
-        };
-
-        template <class T1, class T2, class... Ts>
-        struct overloaded<T1, T2, Ts...> : T1, overloaded<T2, Ts...>
-        {
-            template <class... Us>
-            overloaded(T1 t1, T2 t2, Us... args) : T1(t1), overloaded<T2, Ts...>(t2, args...) {}
-
-            using T1::operator();
-            using overloaded<T2, Ts...>::operator();
-        };
-
-        template <class... Ts>
-        inline overloaded<Ts...> make_overload(Ts... arg)
-        {
-            return overloaded<Ts...>{arg...};
-        }
-    }
-
     template <class L>
     template <class A>
     inline auto xaxis_slice<L>::build_islice(const A& axis) const -> slice_type<A>
     {
         return xtl::visit(
-            detail::make_overload([&axis](const auto& arg) { return slice_type<A>(arg.build_islice(axis)); },
-                                  [&axis](const squeeze_type&) -> slice_type<A> { throw std::runtime_error("build_islice forbidden for squeeze"); }),
+            xtl::make_overload([&axis](const auto& arg) { return slice_type<A>(arg.build_islice(axis)); },
+                               [&axis](const squeeze_type&) -> slice_type<A> { throw std::runtime_error("build_islice forbidden for squeeze"); }),
             m_data);
     }
 
